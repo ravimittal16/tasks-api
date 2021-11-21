f51e9820bb1ba4337435bcf477e42d5f6ee48b4b
Ravi Mittal <ravimittal16@gmail.com>
2021-11-21 21:40:21 +0530

    Added Config Module
    Added Config Module

---
+++ src/transform.interceptor.ts
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
+import { classToPlain } from 'class-transformer';
+import { map, Observable } from 'rxjs';
+
+export class TransformInterceptor implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    return next.handle().pipe(map((data) => classToPlain(data)));
+  }
+}
